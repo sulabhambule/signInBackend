2af224f9c352916a07bd077724f20a95bd8ad204
Sulabh <sulabhambule@gmail.com>
2024-09-16 18:36:30 +0530

    adddd

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: "https://sign-in-frontend-delta.vercel.app/signup",
+    origin: "https://sign-in-frontend-delta.vercel.app/sign-up",
     // origin: "*",
     methods: ["POST", "GET"],
     credentials: true, 
