844189c9d4957bc05632fd0c1757f11a6441012c
Sulabh <sulabhambule@gmail.com>
2024-09-16 19:21:28 +0530

    add

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ app.use(
   })
 );
 
+app.options('*', cors());
+
+
 app.use(express.json({ limit: "16kb" })); 
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); 
 app.use(express.static("public")); 
